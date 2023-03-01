cdfe43c14ed10d45be1cbfdfec534d69351b1833
kscho <kscho@Ubuntu22.04.1.myguest.virtualbox.org>
2023-02-28 19:55:31 -0800

    First commit

---
+++ distSense.py
@@ -0,0 +1,15 @@
+# this is a python program that uses libraries from Dexter Industries
+# to interface with 
+# 1. Grove Ultrasonic Ranger (plug into any digital port)
+# 2. Grove Rotary Angle Sensor (aka potentiometer, plug into any analog port)
+# 3. Grove LCD RGB Backlight (can be plugged into any i2c port)
+
+# Your task is to write a program that
+# 1. Lets you set a threshold distance by turning the rotary angle sensor
+# 2. Measures the distance to an object using the ultrasonic ranger
+# 3. Determines whether the object is within the threshold distance
+# Your program will also print out to the LCD screen three pieces of information:
+# 1. Current threshold value (top line)
+# 2. “OBJ PRES” (top line) if the ultrasonic ranger output falls below the threshold value
+# 3. Current ultrasonic ranger measurement (bottom line)
+
